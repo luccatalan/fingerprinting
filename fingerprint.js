ba84c4a4ee66baee37fa2ebf681c656bb224b140
Catalan Luc <luc.catalan@gmail.com>
2018-10-14 20:07:16 +0200

    Modif port

--- fingerprint.js
+++ fingerprint.js
@@ -82,7 +82,7 @@ app.get('/getResults', function(req, res) {
     });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT, function () {
    var host = server.address().address
    var port = server.address().port
  
